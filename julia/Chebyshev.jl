7ecc5855e051146e4d99d6948eb2ec5be712ae3a
Tzu-Yao Huang (Mac) <tzuyao.jason.huang@gmail.com>
2025-05-31 19:40:38 +0200

    Create module Chebyshev to generate differential matrix of Chebyshev collocation method

---
+++ julia/Chebyshev.jl
@@ -0,0 +1,44 @@
+module Chebyshev
+
+using LinearAlgebra
+
+"""
+    ChebDiffMat(N)
+
+Compute Chebyshev differention matrix and Chebyshev collocation point of the size `N+1`
+"""
+function ChebDiffMat(N)
+    if N==0 
+        return zeros(1,1), [1]
+    end
+
+    c(ii) = ifelse(0<ii<N, 1, 2)
+
+    D = zeros(N+1, N+1)
+    x = cos.((0:N) .* (π / N))
+
+    # Off-diagonal
+    for i∈0:N
+        for j∈i+1:N
+            I, J = i+1, j+1
+            # upper triangle
+            D[I,J] = c(i)/c(j)*((-1)^(i+j))/(x[I]-x[J])
+            # lower triangle
+            D[N+2-I,N+2-J] = -D[I,J]
+        end
+    end
+    
+    # diagonal
+    D[1,1] = (2N^2+1)/6
+    D[N+1,N+1] = -D[1,1]
+    for i∈1:N-1 
+        I=i+1
+        D[I,I] = -x[I]/(2*(1-x[I]^2)) 
+    end
+
+    return D, x
+end
+
+export ChebDiffMat
+
+end
